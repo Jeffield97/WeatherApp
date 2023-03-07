ea547a54d94d2d41d8cc9c7aacd54d80ecea5bc3
Dell <jchariguaman@gmail.com>
2023-03-07 18:59:22 -0500

    Version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './assets/components/Card/Card';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="gradient-radial min-h-screen h-screen flex justify-start items-center flex-col">
+      <Card></Card>
+    </div>
+  );
+};
+
+export default App;
