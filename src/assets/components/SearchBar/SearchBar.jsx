31ae706c505116ec5d16b3a0c2b61d13ce748d39
Dell <jchariguaman@gmail.com>
2023-03-07 19:24:16 -0500

    Search fixed v1

--- src/assets/components/SearchBar/SearchBar.jsx
+++ src/assets/components/SearchBar/SearchBar.jsx
@@ -4,7 +4,7 @@ const SearchBar = ({ setLat, setLon, getWeather }) => {
   const searchBycity = async (cityName) => {
     try {
       const res = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=00ce3cbe5164b0438313a3f261362d8a`,
+        `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=00ce3cbe5164b0438313a3f261362d8a`,
       );
       const cityInfo = await res.json();
 
